b214a077cfc6db28d802bbe7fb876f8857cf7dc9
slurrps-mcgee <slurrpsmcgee@gmail.com>
2025-09-07 22:03:32 -0400

    Frontend

---
+++ snippy/frontend/src/app/shared/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule, DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  // Inject the AuthService to enable authentication features.
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+
+  login() {
+    this.auth.loginWithRedirect({appState: { target: '/userhome' }});
+  }
+
+  logout() {
+    this.auth.logout({ logoutParams: {returnTo: window.location.origin + '/home'} });
+  }
+}
---
+++ snippy/frontend/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from "../login/login.component";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [LoginComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
