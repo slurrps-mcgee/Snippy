6237ec9d29f639cf5ac382e81b4e6f36ddfd6535
slurrps-mcgee <slurrpsmcgee@gmail.com>
2025-09-30 02:45:07 -0400

    remove frontend

--- snippy/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './core/pages/home/home.component';
-import { AuthGuard } from './shared/services/auth.guard';
-
-export const routes: Routes = [
-// {path: '', redirectTo: '/home', pathMatch: 'full' },
-{path: 'home', component: HomeComponent},
-{ path: '**', redirectTo: 'home' } // catch-all -> home
-];
